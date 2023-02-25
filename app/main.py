914caace37ec0d0b32fff59b4cb1f945d8efa7b2
Roseford <obianujuofodu@gmail.com>
2023-02-25 01:05:02 +0100

    added CORS permission

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.routers import crypto
 from app.routers import stocks
@@ -6,6 +7,13 @@ from app.routers import news
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(news.router)
 app.include_router(crypto.router)
 app.include_router(stocks.router)
